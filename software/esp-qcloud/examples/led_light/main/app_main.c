d115d065cd57aafc648d41ceed103182afcee32a
ltpjob@163.com <ltpjob@163.com>
2022-04-15 17:26:26 +0800

    【mod】添加了eth并测试了，唤醒功能，已经是半成品状态，可以用了。。

--- software/esp-qcloud/examples/led_light/main/app_main.c
+++ software/esp-qcloud/examples/led_light/main/app_main.c
@@ -22,7 +22,109 @@
 #include "esp_bt.h"
 #endif
 
+#include "esp_netif.h"
+#include "esp_eth.h"
+#include "driver/gpio.h"
+#include "esp_event.h"
+#include "lwip/err.h"
+#include "lwip/sockets.h"
+#include "lwip/sys.h"
+#include <lwip/netdb.h>
+
+#define CONFIG_EXAMPLE_ETH_MDC_GPIO 23
+#define CONFIG_EXAMPLE_ETH_MDIO_GPIO 18
+#define CONFIG_EXAMPLE_ETH_PHY_RST_GPIO 5
+#define CONFIG_EXAMPLE_ETH_PHY_ADDR 0
+
 static const char *TAG = "app_main";
+
+/** Event handler for Ethernet events */
+static void eth_event_handler(void *arg, esp_event_base_t event_base,
+                              int32_t event_id, void *event_data)
+{
+    uint8_t mac_addr[6] = {0};
+    /* we can get the ethernet driver handle from event data */
+    esp_eth_handle_t eth_handle = *(esp_eth_handle_t *)event_data;
+
+    switch (event_id) {
+    case ETHERNET_EVENT_CONNECTED:
+        esp_eth_ioctl(eth_handle, ETH_CMD_G_MAC_ADDR, mac_addr);
+        ESP_LOGI(TAG, "Ethernet Link Up");
+        ESP_LOGI(TAG, "Ethernet HW Addr %02x:%02x:%02x:%02x:%02x:%02x",
+                 mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+        break;
+    case ETHERNET_EVENT_DISCONNECTED:
+        ESP_LOGI(TAG, "Ethernet Link Down");
+        break;
+    case ETHERNET_EVENT_START:
+        ESP_LOGI(TAG, "Ethernet Started");
+        break;
+    case ETHERNET_EVENT_STOP:
+        ESP_LOGI(TAG, "Ethernet Stopped");
+        break;
+    default:
+        break;
+    }
+}
+
+static int WOL(uint8_t *mac_addr) {
+    struct sockaddr_in dest_addr;
+    int addr_family = 0;
+    int ip_protocol = 0;
+    char *dst_host = "255.255.255.255";
+    uint16_t dst_port = 9;
+    uint8_t wol_buf[102] = {0};
+
+    dest_addr.sin_addr.s_addr = inet_addr(dst_host);
+    dest_addr.sin_family = AF_INET;
+    dest_addr.sin_port = htons(dst_port);
+    addr_family = AF_INET;
+    ip_protocol = IPPROTO_IP;
+
+    int sock = socket(addr_family, SOCK_DGRAM, ip_protocol);
+    if (sock < 0) {
+        ESP_LOGE(TAG, "Unable to create socket: errno %d", errno);
+    }
+    ESP_LOGI(TAG, "Socket created, sending to %s:%d", dst_host, dst_port);
+    for(int i=0; i<6; i++){
+        wol_buf[i] = 0xFF;
+    }
+    int offset = 6;
+    for(int i=0; i<16; i++){
+        for(int j=0; j<6; j++){
+            wol_buf[offset+j] = mac_addr[j];
+        }
+        offset += 6;
+    }
+    int err = sendto(sock, wol_buf, sizeof(wol_buf), 0, (struct sockaddr *)&dest_addr, sizeof(dest_addr));
+    if (err < 0) {
+        ESP_LOGE(TAG, "Error occurred during sending: errno %d", errno);
+    }
+    ESP_LOGI(TAG, "Message sent");
+    if (sock != -1) {
+        ESP_LOGE(TAG, "Shutting down socket");
+        shutdown(sock, 0);
+        close(sock);
+    }
+
+    return 0;
+}
+
+
+/** Event handler for IP_EVENT_ETH_GOT_IP */
+static void got_ip_event_handler(void *arg, esp_event_base_t event_base,
+                                 int32_t event_id, void *event_data)
+{
+    ip_event_got_ip_t *event = (ip_event_got_ip_t *) event_data;
+    const esp_netif_ip_info_t *ip_info = &event->ip_info;
+
+    ESP_LOGI(TAG, "Ethernet Got IP Address");
+    ESP_LOGI(TAG, "~~~~~~~~~~~");
+    ESP_LOGI(TAG, "ETHIP:" IPSTR, IP2STR(&ip_info->ip));
+    ESP_LOGI(TAG, "ETHMASK:" IPSTR, IP2STR(&ip_info->netmask));
+    ESP_LOGI(TAG, "ETHGW:" IPSTR, IP2STR(&ip_info->gw));
+    ESP_LOGI(TAG, "~~~~~~~~~~~");
+}
 
 /* Callback to handle commands received from the QCloud cloud */
 static esp_err_t light_get_param(const char *id, esp_qcloud_param_val_t *val)
@@ -50,6 +152,8 @@ static esp_err_t light_set_param(const char *id, const esp_qcloud_param_val_t *v
 
     if (!strcmp(id, "power_switch")) {
         err = light_driver_set_switch(val->b);
+        uint8_t mac[6] = {0xB0, 0x7B, 0x25, 0x16, 0x5A, 0x2E};
+        WOL(mac);
     } else if (!strcmp(id, "value")) {
         err = light_driver_set_value(val->i);
     } else if (!strcmp(id, "hue")) {
@@ -162,6 +266,19 @@ static esp_err_t get_wifi_config(wifi_config_t *wifi_cfg, uint32_t wait_ms)
 
 void app_main()
 {
+
+    // // Initialize TCP/IP network interface (should be called only once in application)
+    // ESP_ERROR_CHECK(esp_netif_init());
+    // // Create default event loop that running in background
+    // ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+
+
+    // while(1)
+    // {
+    //     vTaskDelay(1000 / portTICK_RATE_MS);
+    // }
+
     /**
      * @brief Add debug function, you can use serial command and remote debugging.
      */
@@ -224,6 +341,30 @@ void app_main()
     ESP_ERROR_CHECK(esp_qcloud_wifi_init());
     ESP_ERROR_CHECK(esp_event_handler_register(QCLOUD_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL));
 
+    esp_netif_config_t cfg = ESP_NETIF_DEFAULT_ETH();
+    esp_netif_t *eth_netif = esp_netif_new(&cfg);
+
+    // Init MAC and PHY configs to default
+    eth_mac_config_t mac_config = ETH_MAC_DEFAULT_CONFIG();
+    eth_phy_config_t phy_config = ETH_PHY_DEFAULT_CONFIG();
+
+    phy_config.phy_addr = CONFIG_EXAMPLE_ETH_PHY_ADDR;
+    phy_config.reset_gpio_num = CONFIG_EXAMPLE_ETH_PHY_RST_GPIO;
+    mac_config.smi_mdc_gpio_num = CONFIG_EXAMPLE_ETH_MDC_GPIO;
+    mac_config.smi_mdio_gpio_num = CONFIG_EXAMPLE_ETH_MDIO_GPIO;
+    esp_eth_mac_t *mac = esp_eth_mac_new_esp32(&mac_config);
+    esp_eth_phy_t *phy = esp_eth_phy_new_lan87xx(&phy_config);
+    esp_eth_config_t config = ETH_DEFAULT_CONFIG(mac, phy);
+    esp_eth_handle_t eth_handle = NULL;
+    ESP_ERROR_CHECK(esp_eth_driver_install(&config, &eth_handle));
+    /* attach Ethernet driver to TCP/IP stack */
+    ESP_ERROR_CHECK(esp_netif_attach(eth_netif, esp_eth_new_netif_glue(eth_handle)));
+
+    // Register user defined event handers
+    ESP_ERROR_CHECK(esp_event_handler_register(ETH_EVENT, ESP_EVENT_ANY_ID, &eth_event_handler, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_ETH_GOT_IP, &got_ip_event_handler, NULL));
+    ESP_ERROR_CHECK(esp_eth_start(eth_handle));
+
     /**
      * @brief Get the router configuration
      */
@@ -242,4 +383,5 @@ void app_main()
     ESP_ERROR_CHECK(esp_qcloud_iothub_init());
     ESP_ERROR_CHECK(esp_qcloud_iothub_start());
     ESP_ERROR_CHECK(esp_qcloud_iothub_ota_enable());
+
 }
